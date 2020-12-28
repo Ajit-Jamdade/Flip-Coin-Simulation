d1997ba2a55b2ce749fcfe1594b624122c5685c6
Ajit-Jamdade <ajitjamdade0@gmail.com>
2020-12-28 15:54:43 +0530

    Add. Added code for no. of times head and tail has won.

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -4,13 +4,24 @@ echo "***** Welcome to Flip Coin Simulation Script. *****"
 
 isHead=0
 isTail=1
+headCount=0
+tailCount=0
 
+read -p "How many times you want to flip the coin : " flipCount
 
-flipResult=$((RANDOM%2))
+for (( i=1; i<=$flipCount; i++ ))
+do
+	flipResult=$((RANDOM%2))
 
-if [ $flipResult -eq $isHead ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+	if [ $flipResult -eq $isHead ]
+	then
+		echo "Heads"
+                headCount=$((headCount+1))
+	else
+		echo "Tails"
+                tailCount=$((tailCount+1))
+	fi
+done
+
+echo "Head has won "$headCount" times."
+echo "Tail has won "$tailCount" times."
