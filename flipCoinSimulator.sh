44b40b2b3756064dbeb885b0c3edad861827e5e0
Ajit-Jamdade <ajitjamdade0@gmail.com>
2020-12-28 15:27:04 +0530

    Add. Added RANDOM function to find Heads and Tails.

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,3 +1,16 @@
 #! /bin/bash -x
 
 echo "***** Welcome to Flip Coin Simulation Script. *****"
+
+isHead=0
+isTail=1
+
+
+flipResult=$((RANDOM%2))
+
+if [ $flipResult -eq $isHead ]
+then
+	echo "Heads"
+else
+	echo "Tails"
+fi
